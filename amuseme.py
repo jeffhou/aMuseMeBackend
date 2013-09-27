65f29c6564db2263b5c4d2e5f2bb90f97098e90c
Michael Lai <themichaellai@gmail.com>
2013-09-27 18:02:36 -0400

    Moved frontend to this repo

--- amuseme.py
+++ amuseme.py
@@ -1,10 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='', static_folder='public')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run()
-

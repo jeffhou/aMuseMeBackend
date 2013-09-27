e3981d5b9f51bc823c2008fa78c7a5096f4d74c0
Michael Lai <themichaellai@gmail.com>
2013-09-27 18:49:48 -0400

    Added index / flask niceties

--- amuseme.py
+++ amuseme.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__, static_url_path='', static_folder='public')
 
 @app.route('/')
-def hello_world():
-    return app.send_static_file('index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/picker')
+def picker():
+    return app.send_static_file('picker.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=os.environ.get('DEBUG', True),
+            port=int(os.environ.get('PORT', 5000)))

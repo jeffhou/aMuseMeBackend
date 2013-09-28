33ac81038c359bb0dc360b5617c6d42a5c0f2db0
Michael Lai <themichaellai@gmail.com>
2013-09-28 18:19:03 -0400

    Include song data in random route

--- itunes.py
+++ itunes.py
@@ -5,8 +5,8 @@ def search(search_term):
     res = requests.get('http://itunes.apple.com/search?term=%s' % search_term)
     return res.json().get('results', [])
 
-def lookup(artist_id):
-    res = requests.get('http://itunes.apple.com/lookup?id=%s' % artist_id)
+def lookup(atom_id):
+    res = requests.get('http://itunes.apple.com/lookup?id=%s' % atom_id)
     if res.json()['resultCount'] >= 1:
         return res.json()['results'][0]
     return {}

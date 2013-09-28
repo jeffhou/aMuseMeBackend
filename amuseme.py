8cc6e61aa75e7e651055684880d108da3589dd5e
Michael Lai <themichaellai@gmail.com>
2013-09-27 20:49:44 -0400

    lastfm artist detail api

--- amuseme.py
+++ amuseme.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from werkzeug.contrib.cache import SimpleCache
 import os
+import lastfm
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='public')
+
+cache = SimpleCache()
 
 @app.route('/')
 def index():
@@ -10,6 +15,18 @@ def index():
 @app.route('/picker')
 def picker():
     return app.send_static_file('picker.html')
+
+@app.route('/api/artist')
+def get_artist():
+    artist_name = request.args.get('artist')
+    if artist_name:
+        artist = cache.get('artist:%s' % artist_name)
+        if artist is None:
+            artist = lastfm.get_artist(artist_name)
+            cache.set('artist:%s' % artist_name, artist, timeout=5*60)
+        return jsonify(artist)
+    else:
+        return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug=os.environ.get('DEBUG', True),
---
+++ lastfm.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from werkzeug.contrib.cache import SimpleCache
+
+#constants
+API_KEY = "188d3cd487e493f17365888e2d6adf9c"
+SECRET = "565f4aa831b8cf35e4693f448aa81bc4"
+
+def get_artist(artist_name):
+    r = requests.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=%s&api_key=%s&format=json" % (artist_name, API_KEY))
+    artist_json_dict = r.json()['artist']
+    return artist_json_dict
+
+
+def get_track(track_name, artist_name):
+    r = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key="+API_KEY+"&artist="+artist_name+"&track="+track_name+"&format=json")
+    track_json_dict = r.json()['track']
+    return track_json_dict
